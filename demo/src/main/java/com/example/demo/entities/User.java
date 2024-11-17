c8b3d0c07f97cc509ef61d2fa8b24cfd966e57fc
Juando26030 <juando02603@gmail.com>
2024-11-17 00:03:33 -0500

    nombre tabla users cambio por nombre reservado

--- demo/src/main/java/com/example/demo/entities/User.java
+++ demo/src/main/java/com/example/demo/entities/User.java
@@ -16,6 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
