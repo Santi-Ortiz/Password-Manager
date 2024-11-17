e1eaa3e622fe265b3f500b3f188bd77eb536de5c
Juando26030 <juando02603@gmail.com>
2024-11-17 00:11:47 -0500

    dbinit class creado

---
+++ demo/src/main/java/com/example/demo/init/DBInitializer.java
@@ -0,0 +1,21 @@
+package com.example.demo.init;
+
+import com.example.demo.entities.*;
+import com.example.demo.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class DBInitializer implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
